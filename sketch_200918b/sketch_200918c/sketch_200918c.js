b5f42f03f5d6c26224f443674fda8fe133ced440
kimjhoma <70741524+kimjhoma@users.noreply.github.com>
2020-09-18 16:06:14 +0900

    Create sketch_200918c.js

---
+++ sketch_200918b/sketch_200918c/sketch_200918c.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(500, 500);
+  for(let i = 0; i<=500; i +=10){
+  line(0, i ,500, i);
+  }
+  for(let i = 0; i<=500; i +=10){
+  line(i, 0, i, 500 );
+  }
+}
+
+
+function draw() {
+
+}
