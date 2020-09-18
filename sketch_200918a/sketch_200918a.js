187cd653dca65c2b2f45e85eb2bc0f5d89963e72
kimjhoma <70741524+kimjhoma@users.noreply.github.com>
2020-09-18 14:09:14 +0900

    re

---
+++ sketch_200918a/sketch_200918a.js
@@ -0,0 +1,14 @@
+let red = 255;
+let green = 255;
+let blue = 0;
+
+function setup() {
+  createCanvas(500, 500);
+}
+
+
+function draw() {
+noStroke(0, 255, 255);
+   fill(255, 0, 0);
+   ellipse(250, 250, 300, 300);
+}
